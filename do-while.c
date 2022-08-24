4bc06f4fffa215ddb64704ff644dfa0e0e51ed6f
MAHADRK <mahadraza79@gmail.com>
2022-08-24 18:53:37 +0200

    do-while loop

---
+++ do-while.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<conio.h>
+#include <math.h>
+
+int main()
+{
+    int x = 0;
+    // do perform the task first then check condition
+    do {
+        printf("this is do %d\n",x);
+        x = x + 1;
+    }
+    while(x<=3);
+
+}
